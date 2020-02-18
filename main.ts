8f3f7fde2c9e042efc98bfcd5f119dcd576c814e
W3404 <W3404@lab.aca>
2020-02-18 16:51:16 -0500

    v2

--- main.ts
+++ main.ts
@@ -1,53 +1,29 @@
-import express =  require("express");
+import express = require("express");
 
-<<<<<<< HEAD
 const PORT = 3000
-=======
-export class Calculadora
-{
-    sumar(op1:number, op2 : number)
-    {
-        return op1 + op2
-    }
-}
->>>>>>> 1698b347ecd9bfd29d033a0c7083459dfb09c752
 
-let main = () =>
-{
-    let app : express.Application = express()
+let main = () => {
+    let app: express.Application = express()
 
     app.set('view engine', 'pug')
     app.set('views', './views')
 
-    app.get('/calculadora', function(req: any, res : any){
+    app.get('/calculadora', function (req: any, res: any) {
         res.render('calculadora', {
-            suma : "0"
+            suma: "0"
         })
     })
-    app.get('/sumar', function(req: any, res : any){
-        let op1 : string = req.query.op1
-        let op2 : string = req.query.op2
-<<<<<<< HEAD
+    app.get('/sumar', function (req: any, res: any) {
+        let op1: string = req.query.op1
+        let op2: string = req.query.op2
         res.render('calculadora', {
-            suma : parseInt(op1) + parseInt(op2)
-=======
-        let calculadora = new Calculadora()
-        res.render('calculadora', {
-            suma : calculadora.sumar(parseInt(op1), parseInt(op2))
->>>>>>> 1698b347ecd9bfd29d033a0c7083459dfb09c752
+            suma: parseInt(op1) + parseInt(op2)
         })
     })
 
     // Iniciamos la ejecucion del server
-<<<<<<< HEAD
-    app.listen(PORT, ()=>{
+    app.listen(PORT, () => {
         console.log(`Server running in port ${PORT}`);
-=======
-    let puerto = process.env.PORT || 3000
-
-    app.listen(puerto, ()=>{
-        console.log(`Server running in port ${puerto}`);
->>>>>>> 1698b347ecd9bfd29d033a0c7083459dfb09c752
     })
 }
 
